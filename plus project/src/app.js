1a569268b555e08a31ade346c8896df719a18a9f
Mmaserufe Sekalo <maserufec@gmail.com>
2024-01-28 12:20:10 +0200

    background image, css and dynamic html from js

---
+++ plus project/src/app.js
@@ -0,0 +1,58 @@
+// api response integration to html
+
+// api integration
+function cityInfo(response) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let temperature = document.querySelector(".temperature");
+  let description = document.querySelector(".description");
+  let humidity = document.querySelector(".humidity");
+  let wind = document.querySelector(".wind");
+  let icon = document.querySelector(".icon");
+  let iconCode = response.data.weather[0].icon;
+  let date = new Date(response.data.dt * 1000);
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  if (mins < 10){
+    mins = `0${mins}`
+  }
+  let day = date.getDay();
+  let currentDay = document.querySelector(".current-day");
+  let time = document.querySelector(".time");
+
+  console.log(date.getMinutes());
+
+  time.innerHTML = `${hours}:${mins}`;
+  currentDay.innerHTML = days[day];
+  icon.setAttribute("src", `https://openweathermap.org/img/w/${iconCode}.png`);
+  wind.innerHTML = response.data.wind.speed;
+  humidity.innerHTML = response.data.main.humidity;
+  description.innerHTML = response.data.weather[0].description;
+  temperature.innerHTML = Math.round(response.data.main.temp);
+}
+function searchCity(city) {
+  let key = "fea2efcd3e02d8f02338366e2c372f87";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+
+  axios.get(apiUrl).then(cityInfo);
+}
+
+//handle submition
+function handleSubmit(event) {
+  event.preventDefault();
+  let input = document.querySelector(".search-box");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = input.value;
+  let city = input.value;
+  searchCity(city);
+}
+//form submit event listener
+let formSubmit = document.querySelector("#submit");
+formSubmit.addEventListener("click", handleSubmit);
